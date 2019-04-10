93ea18671f433bf8fef8321a63363091aea87636
lixiaobing04 <lixiaobing04@meituan.com>
2019-04-10 21:48:25 +0800

    修改

--- MLFromScratchWithScala/Example/src/main/scala/com/scratch/ml/Preprocessing/PrincipalComponentAnalysis.scala
+++ MLFromScratchWithScala/Example/src/main/scala/com/scratch/ml/Preprocessing/PrincipalComponentAnalysis.scala
@@ -1,4 +1,4 @@
-package com.scratch.ml.Preprocession
+package com.scratch.ml.Preprocessing
 
 import breeze.linalg.DenseMatrix
 import com.scratch.ml.model.preprocess.PCA
@@ -7,7 +7,7 @@ object PrincipalComponentAnalysis {
 
   def main(args: Array[String]): Unit = {
     val x = DenseMatrix((1.0, 2.0, 1.1), (2.0, 4.0, 6.0), (10.0, 20.0, 3.0))
-    val a = new PCA().transform(x, 1)
+    val a = PCA.transform(x, 1)
     println(a)
   }
 
